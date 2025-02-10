e45f7b074a4a4a7525155e9783c3bebc09a84d65
Your Name <your.email@example.com>
2025-02-10 13:59:22 +0530

    Initilized spring boot backend

---
+++ springboot_backend/src/main/java/com/yamu/backend/YamuApplication.java
@@ -0,0 +1,16 @@
+package com.yamu.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YamuApplication {
+
+	public static void main(String[] args) {
+
+//		SpringApplication.run(YamuApplication.class, args);
+		System.out.println("Hello World");
+
+	}
+
+}
---
+++ springboot_backend/src/test/java/com/yamu/backend/YamuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yamu.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YamuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
