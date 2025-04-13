5dd00755db485d7498a938de20fce1ed3e053414
Tharuka Deshan <deshanght.22@uom.lk>
2025-04-13 13:37:27 +0530

    new

--- yamu_frontend/src/App.jsx
+++ yamu_frontend/src/App.jsx
@@ -6,9 +6,7 @@ import Register from "./pages/Register.jsx";
 import Home from "./pages/Home.jsx";
 import ProtectedRoute from './routes/ProtectedRoute.jsx';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Go from './pages/Go.jsx';
-import TravellerRegistration from "./components/Registration/TravellerRegistration.jsx";
-import TourGuideRegistration from "./components/Registration/TourGuideRegistration.jsx";
+
 import WeatherPage from "./pages/WeatherPage.jsx";
 import Weather from "./components/Weather.jsx";
 import TravelerDashboard from "./pages/TravelerDashboard.jsx";
@@ -24,11 +22,10 @@ const App = () => {
       <Router>
         <Routes>
           {/* Public Routes */}
+          <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/" element={<Login />} />
-          <Route path="/traveller-register" element={<TravellerRegistration />} />
-          <Route path="/guide-register" element={<TourGuideRegistration />} />
+          
           <Route path="/map" element={<Map />} />
           <Route path="/weather" element={<WeatherPage />} />
           <Route path="/weather-details" element={<Weather />} />
@@ -37,7 +34,7 @@ const App = () => {
           {/* Protected Routes */}
           <Route element={<ProtectedRoute />}>
             <Route path="/home" element={<Home />} />
-            <Route path="/go" element={<Go />} />
+
             <Route path="/traveler-dashboard" element={<TravelerDashboard />} />
             <Route path="/guide-dashboard" element={<GuideDashboard />} />
             <Route path="/guide-review" element={<GuideReview />} />
